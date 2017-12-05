f175de755b9631107b40f24a2dd243096a11ecc8
Yohan Boutin <yohan.bout1@gmail.com>
2017-12-06 00:07:46 +0100

    finalize alpha, and create demo page

---
+++ README.md
@@ -0,0 +1,4 @@
+#MP3Gain.js - normalize MP3 file into the browser
+
+- http://mp3gain.sourceforge.net/
+- https://www.mankier.com/1/mp3gain
---
+++ src/mp3gain/MP3Gain.ts
@@ -0,0 +1,82 @@
+/// <reference path="../../node_modules/emloader/src/emloader/Emloader.ts" />
+/// <reference path="../../typings/index.d.ts" />
+
+namespace mp3gain {
+	export class MP3Gain extends emloader.event.EventEmiter {
+    static ON_STDERROR: string =  'onstderror'
+    static ON_STDOUT: string = 'onstdout'
+
+    /**
+     * Holds files to process on run
+     */
+    private files: Array<emloader.IFile> = []
+
+    /**
+     * MP3 path inside emscripten FS
+     */
+    private mp3Path: string = '/mp3'
+
+		constructor(private binpath: string) {
+      super()
+    }
+
+		public addFile(file: emloader.IFile): void {
+			this.files.push(file)
+		}
+
+		public addFiles(files: Array<emloader.IFile>): void {
+			files.forEach((file) => this.addFile(file))
+    }
+
+    public getOriginalFiles(): Array<emloader.IFile> {
+      return this.files
+    }
+
+		public loadFile(name, url): Promise<emloader.IFile> {
+			return emloader.helper.FileLoader.loadFile(url, name).then((file) => {
+        this.files.push(file)
+
+        return file
+      })
+    }
+
+    public loadFiles(files: Array<string>): Promise<emloader.IFile[]> {
+      return Promise.all(files.map((file) => {
+        return this.loadFile(file.split('/').reverse()[0], file)
+      }))
+    }
+
+		public run(args: Array<string>|string): Promise<emloader.IFile[]> {
+      return emloader.loadHeadless(this.binpath).then((loader) => {
+        const safeArgs = Array.isArray(args) ? args : args.trim().split(' ')
+
+        loader.FS.mkdir(this.mp3Path)
+
+        this.files.forEach((file) => {
+          loader.addFile(file, this.mp3Path)
+          safeArgs.push(this.mp3Path + '/' + file.name)
+        })
+
+        loader.on(emloader.Emloader.ON_STDOUT, (output: string) => {
+          this.emit(MP3Gain.ON_STDOUT, output)
+        })
+        loader.on(emloader.Emloader.ON_STDERROR, (error: string) => {
+          this.emit(MP3Gain.ON_STDERROR, error)
+        })
+
+        return loader.run(safeArgs).then(() => {
+          return loader.FS.readdir(this.mp3Path).filter((filename) => {
+            return !!this.files.find((file) => file.name === filename)
+          }).map((filename): emloader.IFile => {
+            return {
+              name: filename,
+              data: loader.FS.readFile(this.mp3Path + '/' + filename, {
+                encoding: 'binary',
+              }),
+            }
+          })
+        })
+      })
+		}
+	}
+}
