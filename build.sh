c472d608c77d33d8acd222fd1098f3d9bdb53801
Yohan Boutin <yohan.bout1@gmail.com>
2017-12-07 11:44:19 +0100

    update emaloder deps

--- build.sh
+++ build.sh
@@ -18,8 +18,8 @@ EMMAKE_CMD="docker run --rm -v $MP3GAIN_PATH:/build -w /build $DOCKER_IMAGE /ems
 $EMMAKE_CMD make mp3gain OUTPUTNAME=$OUTPUT_FILENAME
 
 cp "$MP3GAIN_PATH/$OUTPUT_FILENAME" "$OUTPUT_PATH"
-type dist/worker.js > dist/mp3gain-worker.js
-type dist/mp3gain-bin.js >> dist/mp3gain-worker.js
+# type dist/worker.js > dist/mp3gain-worker.js
+# type dist/mp3gain-bin.js >> dist/mp3gain-worker.js
 
 # finally, clean project
 $EMMAKE_CMD make clean OUTPUTNAME=$OUTPUT_FILENAME
