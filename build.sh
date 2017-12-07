d07eac05d6b6425f0007100895153dc1fd43c504
Yohan Boutin <yohan.bout1@gmail.com>
2017-12-07 12:22:31 +0100

    update build.sh, add worker build

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ trap "exit" INT
 ########## ENV VARS ##############
 
 OUTPUT_PATH="dist/"
-OUTPUT_FILENAME="mp3gain-bin.js"
+OUTPUT_BINARY_FILENAME="mp3gain-bin.js"
+OUTPUT_WORKER_FILENAME="mp3gain-worker.js"
 MP3GAIN_PATH="$(pwd)/mp3gain"
 
 # Docker Emscripten compiler
@@ -15,11 +16,16 @@ EMMAKE_PATH="emmake"
 # emmake command inside the docker machine
 EMMAKE_CMD="docker run --rm -v $MP3GAIN_PATH:/build -w /build $DOCKER_IMAGE $EMMAKE_PATH"
 
-$EMMAKE_CMD make mp3gain OUTPUTNAME=$OUTPUT_FILENAME
+# build as a binary
+$EMMAKE_CMD make mp3gain OUTPUTNAME=$OUTPUT_BINARY_FILENAME TARGETOPTIONS="-O3 --memory-init-file 0 -s BUILD_AS_WORKER=0"
+cp "$MP3GAIN_PATH/$OUTPUT_BINARY_FILENAME" "$OUTPUT_PATH"
+$EMMAKE_CMD make clean OUTPUTNAME=$OUTPUT_BINARY_FILENAME
 
-cp "$MP3GAIN_PATH/$OUTPUT_FILENAME" "$OUTPUT_PATH"
-# type dist/worker.js > dist/mp3gain-worker.js
-# type dist/mp3gain-bin.js >> dist/mp3gain-worker.js
+# build as a worker
+cp dist/worker.js "$MP3GAIN_PATH"
+$EMMAKE_CMD make mp3gain OUTPUTNAME=$OUTPUT_WORKER_FILENAME TARGETOPTIONS="-O3 --memory-init-file 0 -s BUILD_AS_WORKER=1 --pre-js worker.js"
+cp "$MP3GAIN_PATH/$OUTPUT_WORKER_FILENAME" "$OUTPUT_PATH"
+$EMMAKE_CMD make clean OUTPUTNAME=$OUTPUT_WORKER_FILENAME
+rm -f "$MP3GAIN_PATH/worker.js"
 
-# finally, clean project
-$EMMAKE_CMD make clean OUTPUTNAME=$OUTPUT_FILENAME
+
